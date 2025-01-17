7c4760fa8fb00572f528383bf7e4b0e26a28a148
Rylie Weaver <rylieweaver9@gmail.com>
2025-01-17 23:05:58 +0000

    cleaned up postraining experiment scripts full first draft

---
+++ examples/pretraining-experiments/posttrain.py
@@ -0,0 +1,127 @@
+# General
+import os
+import json
+import datetime
+import argparse
+from tqdm import tqdm
+import random
+
+# Torch
+import torch
+from torch_geometric.loader import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch_scatter import scatter
+
+# E3NN
+from e3nn import o3
+
+# Chi-Geometry
+from chi_geometry.dataset import load_dataset_json
+from chi_geometry.model import Network, load_model_json, process_batch
+from experiment_utils.model import Wrapped_Network
+from experiment_utils.train_val_test import train_val_test_model_no_accuracies
+
+
+def load_model_pretrained(device, pretrained_dir=None):
+    # Choose model args
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    if pretrained_dir is not None:
+        model_config_path = os.path.join(pretrained_dir, "model_args.json")
+    else:
+        model_config_path = os.path.join(script_dir, "model_config.json")
+    model_args = load_model_json(model_config_path)
+    device = torch.device(
+        "cuda" if model_args["use_cuda"] and torch.cuda.is_available() else "cpu"
+    )
+
+    # Load model
+    base = Network(
+        irreps_in=o3.Irreps(model_args["irreps_in"]),
+        irreps_hidden=o3.Irreps(model_args["irreps_hidden"]),
+        irreps_out=o3.Irreps(model_args["irreps_out"]),
+        irreps_node_attr=o3.Irreps(model_args["irreps_node_attr"]),
+        irreps_edge_attr=o3.Irreps(model_args["irreps_edge_attr"]),
+        layers=model_args["layers"],
+        max_radius=model_args["max_radius"],
+        number_of_basis=model_args["number_of_basis"],
+        radial_layers=model_args["radial_layers"],
+        radial_neurons=model_args["radial_neurons"],
+        num_neighbors=model_args["num_neighbors"],
+        num_nodes=model_args["num_nodes"],
+        reduce_output=model_args["reduce_output"],
+        num_classes=model_args["num_classes"],
+        num_heads=model_args["num_heads"],
+    ).to(device)
+
+    # Load pretrained weights
+    if pretrained_dir is not None:
+        print(f"Loading pretrained weights from: {pretrained_dir}")
+        base.load_state_dict(
+            torch.load(
+                os.path.join(pretrained_dir, "best_model.pt"), map_location=device
+            ),
+            strict=False,
+        )
+
+    # Wrap the pretrained network for the binding affinity task
+    model = Wrapped_Network(base, model_args["num_classes"], 1).to(device)
+
+    # Choose modelname
+    if pretrained_dir is not None:
+        modelname = f"from-pretrained_local_e3nn"
+    else:
+        modelname = f"from-scratch_local_e3nn"
+
+    return model, model_args, modelname
+
+
+def main():
+    # Setup
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Load binding affinity dataset
+    datadir = os.path.join(script_dir, "binding_affinity_data")
+    train_dataset = torch.load(os.path.join(datadir, "train.pt"))
+    val_dataset = torch.load(os.path.join(datadir, "val.pt"))
+    test_dataset = torch.load(os.path.join(datadir, "test.pt"))
+    # Shuffle subsetted datasets
+    torch.manual_seed(42)
+    random.seed(42)
+    random.shuffle(train_dataset)
+    random.shuffle(val_dataset)
+
+    # Args
+    pretrained_dir = f"logs/pretrained_local_e3nn"
+    criterion = torch.nn.MSELoss()
+
+    # Run repeatedly for various dataset size
+    for dataset_size in [100, 316, 1000, 3162, 10000]:
+
+        # Get dataset
+        train = train_dataset[:dataset_size]
+        val = val_dataset[: int(0.125 * dataset_size)]
+        test = test_dataset
+        print(
+            f"SAMPLE NUMBERS: Train: {len(train)}, Val: {len(val)}, Test: {len(test)}"
+        )
+
+        # Train models repeatedly
+        for i in range(5):
+            # Train a model from scratch
+            print(f"Training model from scratch...")
+            model, model_args, modelname = load_model_pretrained(None)
+            train_val_test_model_no_accuracies(
+                model, model_args, train, val, test, criterion, f"logs/{modelname}"
+            )
+            # Train a model from pretrained
+            print(f"Training model from pretrained...")
+            model, model_args, modelname = load_model_pretrained(f"logs/{modelname}")
+            train_val_test_model_no_accuracies(
+                model, model_args, train, val, test, criterion, f"logs/{modelname}"
+            )
+
+    print("Experiment complete.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/pretraining-experiments/pretrain.py
@@ -0,0 +1,102 @@
+# General
+import os
+import json
+import datetime
+
+# Torch
+import torch
+from torch_geometric.loader import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+# E3NN
+from e3nn import o3
+
+# Custom
+from chi_geometry.model import Network, load_model_json
+from experiment_utils import train_val_test_model_accuracies
+
+
+def main():
+    # Setup
+    script_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+    distances = [1, 2, 3]
+
+    # Create datasets
+    # dataset_config_path = os.path.join(script_dir, "dataset_config.json")
+    # create_all_datasets(distances, dataset_config_path)
+
+    # Args
+    model_config_path = os.path.join(script_dir, "model_config.json")
+    model_args = load_model_json(model_config_path)
+    noise = False
+    datadir = "datasets"
+    device = torch.device(
+        "cuda" if model_args["use_cuda"] and torch.cuda.is_available() else "cpu"
+    )
+    ## Increase layers with distance to ensure propagation of chiral information
+    model_args["layers"] = max(distances) + 3
+    ## Higher distances will have a lower proportion of chiral centers, so we weight chiral classifications at higher distances more
+    num_classes = model_args["num_classes"]
+    shift_weights = [0.0] + [num_classes * sum(distances) / len(distances)] * (
+        num_classes - 1
+    )
+    class_weights = list(map(sum, zip(model_args["class_weights"], shift_weights)))
+    class_weights = torch.tensor(class_weights, device=device)
+
+    # Gather Composite Dataset
+    dataset = []
+    for dist in distances:
+        if noise:
+            dataset_path = os.path.join(f"{datadir}/noise-{dist}-distance/dataset.pt")
+        else:
+            dataset_path = os.path.join(f"{datadir}/{dist}-distance/dataset.pt")
+        dataset.extend(torch.load(dataset_path))
+
+    # Shuffle and split dataset
+    torch.manual_seed(42)
+    random.seed(42)
+    random.shuffle(dataset)
+    train_dataset = dataset[: int(0.8 * len(dataset))]
+    val_dataset = dataset[int(0.8 * len(dataset)) : int(0.9 * len(dataset))]
+    test_dataset = dataset[int(0.9 * len(dataset)) :]
+    print(
+        f"SAMPLE NUMBERS: Train: {len(train_dataset)}, Val: {len(val_dataset)}, Test: {len(test_dataset)}"
+    )
+
+    # Model
+    log_dir = f"logs/pretrained_local_e3nn"
+    criterion = torch.nn.CrossEntropyLoss(weight=class_weights)
+    model = Network(
+        irreps_in=o3.Irreps(model_args["irreps_in"]),
+        irreps_hidden=o3.Irreps(model_args["irreps_hidden"]),
+        irreps_out=o3.Irreps(model_args["irreps_out"]),
+        irreps_node_attr=o3.Irreps(model_args["irreps_node_attr"]),
+        irreps_edge_attr=o3.Irreps(model_args["irreps_edge_attr"]),
+        layers=model_args["layers"],
+        max_radius=model_args["max_radius"],
+        number_of_basis=model_args["number_of_basis"],
+        radial_layers=model_args["radial_layers"],
+        radial_neurons=model_args["radial_neurons"],
+        num_neighbors=model_args["num_neighbors"],
+        num_nodes=model_args["num_nodes"],
+        reduce_output=model_args["reduce_output"],
+        num_classes=model_args["num_classes"],
+        num_heads=model_args["num_heads"],
+    ).to(device)
+    print(f"Pre-Training model for distances {distances} and noise {noise}...")
+    train_val_test_model_accuracies(
+        model,
+        model_args,
+        train_dataset,
+        val_dataset,
+        test_dataset,
+        criterion,
+        num_classes,
+        log_dir,
+    )
+
+    print("Experiment complete.")
+
+
+if __name__ == "__main__":
+    main()
