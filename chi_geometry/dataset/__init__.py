d39987285ef842c579564fcbdde39f62e51d7eb9
Rylie Weaver <rylie@MacBook-Pro.local>
2025-01-27 15:15:57 -0500

    fix inits of functions

--- chi_geometry/dataset/__init__.py
+++ chi_geometry/dataset/__init__.py
@@ -1,4 +1,22 @@
 from .utils import load_dataset_json, center_and_rotate_positions
-from .create_dataset import create_dataset
+from .create_dataset import (
+    create_dataset,
+    create_chiral_instance,
+    scalar_triple_product,
+)
+from .debug import (
+    check_classic_configurations,
+    check_simple_configurations,
+    check_crossed_configurations,
+)
 
-__all__ = ["load_dataset_json", "center_and_rotate_positions", "create_dataset"]
+__all__ = [
+    "load_dataset_json",
+    "center_and_rotate_positions",
+    "create_dataset",
+    "create_chiral_instance",
+    "scalar_triple_product",
+    "check_classic_configurations",
+    "check_simple_configurations",
+    "check_crossed_configurations",
+]
